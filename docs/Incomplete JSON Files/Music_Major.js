3a3a128c310f339e95ddaf056376018a34ec58dc
Abigail <aes6h2@mail.missouri.edu>
2017-11-14 18:25:52 -0600

    Put files for Business Minor, Math Minor, IT Major, and Music Major into Incomplete JSON Files folder

---
+++ docs/Incomplete JSON Files/Music_Major.js
@@ -0,0 +1,228 @@
+var json =
+`[
+    {
+        "Department": "Music-Applied Music (MUS_APMS)",
+        "Courses": [
+            {
+                "Title": "MUS_APMS 2455:  Studio Instruction",
+                "GUID": "MUS_APMS2455",
+                "Description": "Credit accepted toward all undergraduate music and music education degrees. May be repeated for credit.",
+                "Hours": "1-5",
+                "Prerequisites": "instructor's consent"
+            },
+            {
+                "Title": "MUS_APMS 3455:  Studio Instruction",
+                "GUID": "MUS_APMS3455",
+                "Description": "Accepted as upperclass credit only in Music Education, music theory, history, or composition.  May be repeated for credit.",
+                "Hours": "1-3",
+                "Prerequisites": "instructor's consent"
+            }
+        ]
+    },
+    {
+        "Department": "Music-Ensemble Courses (MUS_ENS)",
+        "Courses": [
+            {
+                "Title": "MUS_ENS 1841:  Instrumental Ensemble",
+                "GUID": "MUS_ENS1841",
+                "Description": "Provides experience in instrumental performance and repertory. Open to all MU students by audition. May be repeated for credit. Enrollment in Marching Band is limited to a maximum of five semesters. Sections are: Philharmonic Orchestra, Chamber Orchestra, Symphonic Band, Wind Ensemble, Concert Band, Varsity Band, Studio Jazz Ensemble, Jazz Lab Band, Marching Band. Some sections may be graded on an S/U or A-F basis only.",
+                "Hours": "0-1"
+            },
+            {
+                "Title": "MUS_ENS 1842:  Choral Ensemble",
+                "GUID": "MUS_ENS1842",
+                "Description": "Provides experience in choral performance and repertory. Open to all MU students. May be repeated for credit. Sections are: University Singers, Chamber Singers, Choral Union, Vocal Jazz Ensemble, Concert Chorale, Men's Chorus, Women's Chorus. Some sections may be graded on an S/U or A-F basis only.",
+                "Hours": "0-1",
+                "Prerequisites": "Audition"
+            }
+        ]
+    },
+    {
+        "Department": "Music-General (MUS_GENL)",
+        "Courses": [
+            {
+                "Title": "MUS_GENL 1091:  Recital Attendance for Undergraduate Music Majors",
+                "GUID": "MUS_GENL1091",
+                "Description": "Required attendance of eleven music events from the Music Department listing. 0 credit, and may be repeated until the total degree requirement is satisfactorily met. No tuition charged.  Graded on S/U basis only.",
+                "Hours": "0"
+            },
+            {
+                "Title": "MUS_GENL 3085:  Problems in Music",
+                "GUID": "MUS_GENL3085",
+                "Description": "Independent investigation leading to a paper or project. May be repeated for credit. Sections are: Music Theory, Music Composition, Music History, Music Performance/Pedagogy.",
+                "Hours": "1-99",
+                "Prerequisites": "junior standing in Music and instructor's consent. Honors eligibility required"
+            }
+        ]
+    },
+    {
+        "Department": "Music-Music History And Literature (MUS_H_LI)",
+        "Courses": [
+            {
+                "Title": "MUS_H_LI 1322:  Introduction to Music in the United States",
+                "GUID": "MUS_H_LI1322",
+                "Description": "Historical overview of American folk, popular, and fine-art music; emphasis on listening skills.",
+                "Hours": "2"
+            },
+            {
+                "Title": "MUS_H_LI 2307:  History of Western Music I",
+                "GUID": "MUS_H_LI2307",
+                "Description": "Historical survey of selected European practices up to 1700 following a consideration of the major fine-art traditions of the world.",
+                "Hours": "2"
+            },
+            {
+                "Title": "MUS_H_LI 2308:  History of Western Music II",
+                "GUID": "MUS_H_LI2308",
+                "Description": "Historical survey of Western fine-art music from approximately 1700 to the present.",
+                "Hours": "2",
+                "Prerequisites": "Grade of C- or better in MUS_H_LI 1322"
+            },
+            {
+                "Title": "MUS_H_LI 4311W:  Historical Studies in Art Song - Writing Intensive",
+                "GUID": "MUS_H_LI4311W",
+                "Description": "Historical survey of works for solo voice and instruments.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in  MUS_H_LI 2308 and instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4312W:  Historical Studies in Choral Music - Writing Intensive",
+                "GUID": "MUS_H_LI4312W",
+                "Description": "Historical survey of works featuring choral ensembles.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in MUS_H_LI 2308 and instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4313W:  Historical Studies in Opera - Writing Intensive",
+                "GUID": "MUS_H_LI4313W",
+                "Description": "Historical survey of opera.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in  MUS_H_LI 2308 and instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4315W:  Historical Studies in Chamber Music - Writing Intensive",
+                "GUID": "MUS_H_LI4315W",
+                "Description": "Historical survey of works for small ensembles, instrumental and vocal.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in MUS_H_LI 2308 and instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4317W:  Historical Studies in Jazz and Popular Music - Writing Intensive",
+                "GUID": "MUS_H_LI4317W",
+                "Description": "Historical survey of works from the realm of American jazz and popular music.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in MUS_H_LI 2308 and instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4318W:  Studies in World Music - Writing Intensive",
+                "GUID": "MUS_H_LI4318W",
+                "Description": "Advanced systematic study of musical activities in selected world cultures, with the emphasis on developing listening skills and understanding the role of music in a culture.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in MUS_H_LI 2307 and MUS_H_LI 2308"
+            },
+            {
+                "Title": "MUS_H_LI 4320W:  Historical Studies in African-American Music - Writing Intensive",
+                "GUID": "MUS_H_LI4320W",
+                "Description": "Exploration of history and current scholarship in African-American music from the eighteenth to twenty-first centuries. Genres include folk music, religious music, blues, ragtime, jazz, musical theater, art music, R&B, funk, soul, disco, house, hip-hop and rap.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in MUS_H_LI 2308 and Instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4337W:  Music of the Classic Era - Writing Intensive",
+                "GUID": "MUS_H_LI4337W",
+                "Description": "Systematic study of European musical practice from approximately 1750 to 1800.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in  MUS_H_LI 2308 and instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4340W:  Focal Composers - Writing Intensive",
+                "GUID": "MUS_H_LI4340W",
+                "Description": "Systematic study of the works of landmark composers: J.S. Bach, Mozart, Beethoven, Verdi/Wagner, Debussy, or Stravinsky, studied in rotation. Repeatable for up to 6 hours or credit.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in MUS_H_LI 2308 and instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4341W:  Advanced Studies in American Music - Writing Intensive",
+                "GUID": "MUS_H_LI4341W",
+                "Description": "(cross-leveled with MUS_H_LI 7341). Systematic study of the diverse streams of musical practice in the United States from the colonial time to the present.",
+                "Hours": "3",
+                "Prerequisites": "Grade of C- or better in  MUS_H_LI 2308 and instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4350W:  Introduction to Ethnomusicology - Writing Intensive",
+                "GUID": "MUS_H_LI4350W",
+                "Description": "Study of theories, historical development, research methodologies, and practice of ethnomusicology, in an interdisciplinary approach.  Topics include ethnographic research, oral and literate sources, transcription and analysis, critical analysis, and interpretative techniques.",
+                "Hours": "3",
+                "Prerequisites": "grade of C-or better in MUS_H_LI 2308; instructor's consent"
+            },
+            {
+                "Title": "MUS_H_LI 4352W:  Historical Studies in African Music - Writing Intensive",
+                "GUID": "MUS_H_LI4352W",
+                "Description": "(same as BL_STU 4352) Ethnomusicological introduction to the music and culture of countries and ethnic groups in Africa.  Traditional and contemporary popular styles are explored, and influences of Islamic invasions, missionary arrivals, colonial conquests, neo-colonial trends, and globalization.",
+                "Hours": "3",
+                "Prerequisites": "grade of C-or better in MUS_H_LI 2308; instructor's consent"
+            }
+        ]
+    },
+    {
+        "Department": "Music-Music Theory (MUS_THRY)",
+        "Courses": [
+            {
+                "Title": "MUS_THRY 1220:  Syntax, Structure and Style of Music I",
+                "GUID": "MUS_THRY1220",
+                "Description": "Review of fundamentals. Study of rhythm, melody, harmonic, structure and stylistic characteristics of various periods. Application through original composition projects. Prerequisites: none for Music majors; others: instructor's consent.",
+                "Hours": "2",
+                "Prerequisites": "consent required.  Placement by exam"
+            },
+            {
+                "Title": "MUS_THRY 1221:  Syntax, Structure and Style of Music II",
+                "GUID": "MUS_THRY1221",
+                "Description": "Continuation of MUS_THRY 1220. Study of smaller forms and introduction to chromatic harmony.",
+                "Hours": "2"
+            },
+            {
+                "Title": "MUS_THRY 1230:  Aural Training and Sight Singing I",
+                "GUID": "MUS_THRY1230",
+                "Description": "Development of aural and sight singing skills.",
+                "Hours": "2",
+                "Prerequisites": "Grade of C- or better in MUS_THRY 1220"
+            },
+            {
+                "Title": "MUS_THRY 1231:  Aural Training and Sight Singing II",
+                "GUID": "MUS_THRY1231",
+                "Description": "Continuation of MUS_THRY 1230.",
+                "Hours": "2",
+                "Prerequisites": "MUS_THRY 1220"
+            },
+            {
+                "Title": "MUS_THRY 2220:  Syntax, Structure and Style of Music III",
+                "GUID": "MUS_THRY2220",
+                "Description": "Chromatic harmony, variation techniques and contrapuntal genres. Study of traditional forms in instrumental, vocal and choral compositions. Applications through original composition projects.",
+                "Hours": "2",
+                "Prerequisites": "Grade of C- or better in MUS_THRY 2215"
+            },
+            {
+                "Title": "MUS_THRY 2221:  Syntax, Structure and Style of Music IV",
+                "GUID": "MUS_THRY2221",
+                "Description": "Continued study of chromatic harmony and compositions in larger forms. Application through original composition projects.",
+                "Hours": "2",
+                "Prerequisites": "Grade of C- or better in MUS_THRY 1221"
+            },
+            {
+                "Title": "MUS_THRY 2230:  Aural Training and Sight Singing III",
+                "GUID": "MUS_THRY2230",
+                "Description": "Continuation of MUS_THRY 1231. Further development of aural and sight singing skills with an emphasis on chromatic harmony and decorative pitches. Introduction of structural perception.",
+                "Hours": "2",
+                "Prerequisites": "Grade of C- or better in MUS_THRY 2220"
+            },
+            {
+                "Title": "MUS_THRY 2231:  Aural Training and Sight Singing IV",
+                "GUID": "MUS_THRY2231",
+                "Description": "Continuation MUS_THRY 2230.",
+                "Hours": "2",
+                "Prerequisites": "Grade of C- or better in MUS_THRY 1231 and MUS_THRY 2220 or MUS_THRY 2220 concurrently"
+            }
+        ]
+    }
+]`;
+
+var allCourses = JSON.parse(json);
